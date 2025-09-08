8167c56c87722a418143211a09e490244b7803dd
Tanya Vasylovych <tetiana.vasylovych.work@gmail.com>
2025-09-08 17:18:33 +0300

    authprovider

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,33 +1,26 @@
 import Link from "next/link";
 import css from "./Header.module.css";
 import TagsMenu from "../TagsMenu/TagsMenu";
+import AuthProvider from "../AuthProvider/AuthProvider";
+import AuthNavigation from "../AuthNavigation/AuthNavigation";
 
 const Header = () => {
   return (
-    <header className={css.header}>
-      <Link href="/" aria-label="Home">
-        NoteHub
-      </Link>
-      <nav aria-label="Main Navigation">
-        <ul className={css.navigation}>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/sign-in">Login</Link>
-          </li>
-          <li>
-            <Link href="/sign-up">Register</Link>
-          </li>
-          <li>
-            <Link href="/profile">Profile</Link>
-          </li>
-          <li>
-            <TagsMenu />
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <AuthProvider>
+      <header className={css.header}>
+        <Link href="/" aria-label="Home">
+          NoteHub
+        </Link>
+        <nav aria-label="Main Navigation">
+          <ul className={css.navigation}>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <AuthNavigation />
+          </ul>
+        </nav>
+      </header>
+    </AuthProvider>
   );
 };
 
